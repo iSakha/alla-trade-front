f597768e00c2139b55a1325f42fe249e37eeb41f
sakha <sakha60@gmail.com>
2025-11-30 12:56:53 +0300

    commit

--- src/pages/Warehouse/Warehouse.jsx
+++ src/pages/Warehouse/Warehouse.jsx
@@ -4,7 +4,7 @@ import { Container } from "react-bootstrap";
 import axios from "axios";
 import WhTable from "../../components/tables/WhTable";
 
-const URL = "https://sakha.lat/alla"
+const URL = import.meta.env.VITE_API_URL;
 
 
 function Warehouse() {
